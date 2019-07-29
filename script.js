e27af8215e4f4e9b7a6b27d5310cb2c0cc605064
Angelika <maciazekangelika@gmail.com>
2019-07-29 19:58:54 +0200

    add task,remove task, task completed working

--- script.js
+++ script.js
@@ -1,77 +1,88 @@
 class Task {
-  constructor(text, taskId) {
-    this.text = text
-    this.taskId = taskId
+  constructor(text) {
+      this.text = text
+
+
+      this.isCompleted = false
+     
   }
 
 }
+
+
 
 class ToDoList {
   constructor() {
 
-    this.tasks = []
-    this.rowIndex
-    this.deleteTaskButtonIndex = 0
-    this.taskId = 0
-    this.render()
+      this.tasks = []
+     
+      this.render()
   }
+
 
   render() {
 
-    document.body.innerHTML = ''
-    this.promptFormForAddingTask()
-    this.rowIndex = 0
-    const ul = document.createElement('ul')
-    ul.className = 'todo-list'
-    this.tasks.forEach(task => {
-      const li = document.createElement('li')
-      const deleteTaskButton = document.createElement('div')
-      li.className = 'list-row'
-      deleteTaskButton.className = 'delete-task-button'
-      li.id = this.rowIndex++
-      deleteTaskButton.id = this.deleteTaskButtonIndex++
+      document.body.innerHTML = ''
+      this.promptFormForAddingTask()
+     
+      const ul = document.createElement('ul')
+      ul.className = 'todo-list'
+      this.tasks.forEach((task, taskIndex) => {
+          const li = document.createElement('li')
+          const removeTaskButton = document.createElement('div')
+          const removeIcon = document.createTextNode("\u00D7")
 
-      deleteTaskButton.addEventListener('click', () => {
-        console.log('tasks', this.tasks)
-        console.log('row', this.rowIndex)
-        ul.removeChild(li)
+          li.classList.add('task')
+          removeTaskButton.className = 'delete-task-button'
+
+          li.addEventListener('click', (event) => {
+              event.target.classList.add('task--completed')
+              task.isCompleted=true
+          })
+
+          removeTaskButton.addEventListener('click', () => {
+              ul.removeChild(li)
+              this.tasks = this.tasks.slice(0, taskIndex).concat(this.tasks.slice(taskIndex + 1, this.tasks.length))
+              this.render()
+          })
+
+          removeTaskButton.appendChild(removeIcon)
+          li.innerText = task.text
+          if(task.isCompleted){
+              li.style.textDecoration="line-through"
+              li.style.textDecorationColor='light-gray'
+          }
+
+          li.appendChild(removeTaskButton)
+          ul.appendChild(li)
       })
-
-      li.innerText = task.text
-      li.appendChild(deleteTaskButton)
-      ul.appendChild(li)
-    })
-    document.body.appendChild(ul)
+      document.body.appendChild(ul)
 
   }
+
 
   addTaskToList(text) {
-
-    if (text == '' || text == null) {
-      alert("Dodaj zadanie!")
-    } else {
-      this.taskId++
-      this.tasks.push(new Task(text, this.taskId))
-    }
-    this.render()
+      if (text == '' || text == null) {
+          alert("Empty task")
+      } else {
+          this.tasks.push(new Task(text))
+          console.log(this.tasks)
+      }
+      this.render()
   }
-  removeTaskFromList() {
 
-    console.log('removed', this.tasks)
-    this.render()
-  }
 
   promptFormForAddingTask() {
-    const input = document.createElement('input')
-    const button = document.createElement('button')
-    button.innerText = '+'
+      const input = document.createElement('input')
+      const button = document.createElement('button')
+      button.innerText = '+'
 
-    button.addEventListener('click', () => this.addTaskToList(input.value))
-    document.body.appendChild(input)
-    document.body.appendChild(button)
+      button.addEventListener('click', () => this.addTaskToList(input.value))
+      document.body.appendChild(input)
+      document.body.appendChild(button)
 
   }
+
 
 }
 const todo = new ToDoList()
-
